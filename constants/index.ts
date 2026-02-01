ef836bbd01524c907d9447d2c3ded5db4ea8bf54
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-02-01 20:34:14 +0530

    Define skill constants for frontend and backend
    Added frontend, backend, fullstack, and other skill constants.

--- constants/index.ts
+++ constants/index.ts
@@ -12,6 +12,20 @@ export const Skill_Data = [
   { skill_name: "Next js 13", Image: "/next.png", width: 80, height: 80 },
 ];
 
+export const FRONTEND_SKILL = [
+  { skill_name: "Html 5", Image: "/html.png", width: 80, height: 80 },
+  { skill_name: "Css 3", Image: "/css.png", width: 80, height: 80 },
+  { skill_name: "Java Script", Image: "/js.png", width: 65, height: 65 },
+];
+
+export const BACKEND_SKILL = [
+  { skill_name: "Node js", Image: "/node-js.png", width: 80, height: 80 },
+  { skill_name: "Mongo db", Image: "/mongodb.png", width: 40, height: 40 },
+];
+
+export const FULLSTACK_SKILL = [];
+export const OTHER_SKILL = [];
+
 export const SOCIALS = [
   { name: "Telegram", icon: FaTelegramPlane, link: "https://t.me/Shubhams872" },
   { name: "GitHub", icon: RxGithubLogo, link: "https://github.com/Shubhams872-spec" },
@@ -36,7 +50,7 @@ export const FOOTER_DATA = [
 export const PROJECTS = [
   {
     title: "Certified Cybersecurity Educator Professional (CCEP)",
-    description: "Issued by Red Team Leaders on December 7, 2025. This certification validates expertise in secure infrastructure and defense training.",
+    description: "Issued by Red Team Leaders on December 7, 2025. This certification validates my expertise in secure infrastructure and defense training.",
     image: "/cert2.jpg",
     link: "https://github.com/Shubhams872-spec",
   },
@@ -59,7 +73,4 @@ export const LINKS = {
   sourceCode: "https://github.com/Shubhams872-spec/space-portfolio" 
 };
 
-export const FULLSTACK_SKILL = [];
-export const OTHER_SKILL = [];
-export const BACKEND_SKILL = [];
 export const ABOUT_TEXT = "I am Shubham Singh, an 11th-grade student and Frontend Developer from Kanpur. I am currently preparing for NEET while specializing in Cybersecurity and Ethical Hacking.";
