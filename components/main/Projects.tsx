e5d770300325002d35d1e0294be114364f4f662f
Shubham-ai-glitch <shubhamsingh7236002388@gmail.com>
2026-02-08 16:40:18 +0530

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,24 +1,26 @@
-"use client";
 import React from "react";
-import ProjectCard from "../sub/ProjectCard"; // Check karein 'P' aur 'C' capital ho
+import ProjectCard from "../sub/ProjectCard";
 import { PROJECTS } from "@/constants";
 
-export const Projects = () => {
+const Projects = () => {
   return (
-    <section id="projects" className="flex flex-col items-center justify-center py-20">
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        My Achievements & Projects
+    <div className="flex flex-col items-center justify-center py-20" id="projects">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20 text-center">
+        My Achievements
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         {PROJECTS.map((project) => (
-          <ProjectCard
-            key={project.title}
-            src={project.image}
-            title={project.title}
-            description={project.description}
-          />
+          <a key={project.title} href={project.download} download className="z-[50] cursor-pointer hover:scale-[1.02] transition-transform flex-1">
+            <ProjectCard
+              src={project.image}
+              title={project.title}
+              description={project.description}
+            />
+          </a>
         ))}
       </div>
-    </section>
+    </div>
   );
 };
+
+export default Projects;
